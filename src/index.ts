8ebac2ff2221ba3d645a7db493cd849dc96c30b6
Brock McCrory <brockton.mccrory@trojans.dsu.edu>
2021-12-19 17:46:47 -0600

    Prepping for major migration to Twurple

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Discord, { TextChannel } from 'discord.js';
 import { RefreshableAuthProvider, StaticAuthProvider } from 'twitch-auth';
 import { ChatClient } from 'twitch-chat-client';
-import { PubSubClient } from 'twitch-pubsub-client';
+import { PubSubChatModActionMessage, PubSubClient, PubSubSubscriptionMessage } from 'twitch-pubsub-client';
 import { ApiClient } from 'twitch';
 import { promises as fs} from 'fs';
 import * as dotenv from 'dotenv';
@@ -35,7 +35,7 @@ async function main() {
 		}
 		);
 		
-	const twitchChannels = ['spoodah', 'azfura', 'dexter0', 'pacopaco', 'jg7tv', 'sirkenbo'];
+	const twitchChannels = ['spoodah', 'azfura', 'ilybeamic', 'pacopaco', 'jg7tv', 'sirkenbo'];
 	const chatClient = new ChatClient(authProvider, { channels: twitchChannels, logger: { minLevel: 'warning' } });
 	await chatClient.connect()
 	.then(() => {
@@ -88,14 +88,14 @@ async function main() {
 					let botMsg = `PogChamp BITS DONATION!!! PogChamp Thank you so much @${user} for the ${msg.totalBits} bitties! You're too kind! alexiaLove`;
 					publishMessage(channel, botMsg);
 					break;
-				case '#jg7tv':
-					botMsg = `classCheer BITS DONATION!!! classCheer Thank you so much @${user} for the ${msg.totalBits} bitties! You're too kind! jg7tvWee`;
-					publishMessage(channel, botMsg);
-					break;
-				case '#pacopaco':
-					botMsg = `pacopaChamp BITS DONATION!!! pacopaChamp Thank you so much @${user} for the ${msg.totalBits} bitties! You're too kind! pacopaSmile`;
-					publishMessage(channel, botMsg);
-					break;
+				// case '#jg7tv':
+				// 	botMsg = `classCheer BITS DONATION!!! classCheer Thank you so much @${user} for the ${msg.totalBits} bitties! You're too kind! jg7tvWee`;
+				// 	publishMessage(channel, botMsg);
+				// 	break;
+				// case '#pacopaco':
+					// botMsg = `pacopaChamp BITS DONATION!!! pacopaChamp Thank you so much @${user} for the ${msg.totalBits} bitties! You're too kind! pacopaSmile`;
+					// publishMessage(channel, botMsg);
+					// break;
 				default:
 					botMsg = `PogChamp BITS DONATION!!! PogChamp Thank you so much @${user} for the ${msg.totalBits} bitties! You're too kind! alexiaLove`;
 					publishMessage(channel, botMsg);
@@ -108,49 +108,49 @@ async function main() {
 		}
 
 		if(message.toLowerCase() === '!prime') {
-			publishMessage(channel, `If you have Amazon Prime connect your account here -> twitch.amazon.com/prime ! Once you have linked your accounts click the sub button and sub for FREE PogChamp Need help? Click here -> https://goo.gl/VyzHCX cizzHearts`);
+			publishMessage(channel, `If you have Amazon Prime connect your account here -> twitch.amazon.com/prime ! Once you have linked your accounts click the sub button and sub for FREE PogChamp Need help? Click here -> https://goo.gl/VyzHCX kaleiLove`);
 		}
 	});
 
 	chatClient.onSub((channel, user, subInfo) => {
 		if(subInfo.isPrime) {
-			const botMsg = `pokiPrime PRIME SUB!!! pokiPrime @${user} just subscribed using Twitch Prime! Welcome to the squadron <3 spoodGLB`;
-			const chnl = channel.trim();
-			switch (chnl) {
-				case '#spoodah':
-					let botMsg = `pokiPrime PRIME SUB!!! pokiPrime @${user} just subscribed using Twitch Prime! Welcome to the squadron <3 spoodGLB`;
-					publishMessage(channel, botMsg);
-					break;
-				case '#jg7tv':
-					botMsg = `classMob PRIME SUB!!! classMob @${user} just subscribed using Twitch Prime! Welcome to the Sub 7's! dazeriNlove`;
-					publishMessage(channel, botMsg);
-					break;
-				case '#pacopaco':
-					botMsg = `pokiPrime PRIME SUB!!! pokiPrime @${user} just subscribed using Twitch Prime! Welcome to the family pacopaSmile`;
-					publishMessage(channel, botMsg);
-					break;
-				default:
-					botMsg = `pokiPrime PRIME SUB!!! pokiPrime @${user} just subscribed using Twitch Prime! Welcome to the squadron <3 spoodGLB`;
-					publishMessage(channel, botMsg);
-					break;
-			}
+			const botMsg = `xqcPrime PRIME SUB!!! xqcPrime @${user} just subscribed using Twitch Prime! Welcome to the squadron kaleiLove`;
+			// const chnl = channel.trim();
+			// switch (chnl) {
+			// 	case '#spoodah':
+			// 		let botMsg = `pokiPrime PRIME SUB!!! pokiPrime @${user} just subscribed using Twitch Prime! Welcome to the squadron sirken2LOVE`;
+			// 		publishMessage(channel, botMsg);
+			// 		break;
+			// 	case '#jg7tv':
+			// 		botMsg = `classMob PRIME SUB!!! classMob @${user} just subscribed using Twitch Prime! Welcome to the Sub 7's! dazeriNlove`;
+			// 		publishMessage(channel, botMsg);
+			// 		break;
+			// 	case '#pacopaco':
+			// 		botMsg = `pokiPrime PRIME SUB!!! pokiPrime @${user} just subscribed using Twitch Prime! Welcome to the family pacopaSmile`;
+			// 		publishMessage(channel, botMsg);
+			// 		break;
+			// 	default:
+			// 		botMsg = `pokiPrime PRIME SUB!!! pokiPrime @${user} just subscribed using Twitch Prime! Welcome to the squadron sirken2LOVE`;
+			// 		publishMessage(channel, botMsg);
+			// 		break;
+			// }
 			publishMessage(channel, botMsg);
 		}
 		else if(subInfo.plan === '1000') {
 			const chnl = channel.trim();
 			switch(chnl) {
 				case '#spoodah':
-					let botMsg = `spoodHype NEW SUB!!! spoodHype @${user} just subscribed! Welcome to the squadron <3 spoodGLB`;
+					let botMsg = `spoodHype NEW SUB!!! spoodHype @${user} just subscribed! Welcome to the squadron kaleiLove`;
 					publishMessage(channel, botMsg);
 					break;
-				case '#jg7tv':
-					botMsg = `jg7tvHype NEW SUB!! jg7tvHype @${user} just subbed! jg7tvPog7 What color do you want the lights, @{user} ? alexiaPeek`;
-					publishMessage(channel, botMsg);
-					break;
-				case '#pacopaco':
-					botMsg = `pacopaChamp NEW SUB!!! pacopaChamp @${user} just subscribed! Enjoy the emotes and the vibes pacopaSmile`;
-					publishMessage(channel, botMsg);
-					break;
+				// case '#jg7tv':
+				// 	botMsg = `jg7tvHype NEW SUB!! jg7tvHype @${user} just subbed! jg7tvPog7 What color do you want the lights, @{user} ? alexiaPeek`;
+				// 	publishMessage(channel, botMsg);
+				// 	break;
+				// case '#pacopaco':
+				// 	botMsg = `pacopaChamp NEW SUB!!! pacopaChamp @${user} just subscribed! Enjoy the emotes and the vibes pacopaSmile`;
+				// 	publishMessage(channel, botMsg);
+				// 	break;
 				default:
 					botMsg = `PogChamp NEW SUB!!! PogChamp @${user} just subscribed! Welcome to the sub squad and stay a while! alexiaLove`;
 					publishMessage(channel, botMsg);
@@ -164,14 +164,14 @@ async function main() {
 					let botMsg = `spoodHype NEW TIER 2 SUB!!! spoodHype @${user} just subscribed at Tier 2 ($9.99 USD)! THE STREAM IS FREE!!1! alexiaMega`;
 					publishMessage(channel, botMsg);
 					break;
-				case '#jg7tv':
-					botMsg = `jg7tvPog7 NEW TIER 2 SUB!!! jg7tvPog7 @${user} just subscribed at Tier 2 ($9.99 USD)! Thank you so freaking much for the support!!! alexiaLove`;
-					publishMessage(channel, botMsg);
-					break;
-				case '#pacopaco':
-					botMsg = `alexiaWow NEW TIER 2 SUB!!! alexiaWow @${user} just subscribed at Tier 2 ($9.99 USD)! Thank you so freaking much for the support!!! pacopaSmile`;
-					publishMessage(channel, botMsg);
-					break;
+				// case '#jg7tv':
+				// 	botMsg = `jg7tvPog7 NEW TIER 2 SUB!!! jg7tvPog7 @${user} just subscribed at Tier 2 ($9.99 USD)! Thank you so freaking much for the support!!! alexiaLove`;
+				// 	publishMessage(channel, botMsg);
+				// 	break;
+				// case '#pacopaco':
+				// 	botMsg = `alexiaWow NEW TIER 2 SUB!!! alexiaWow @${user} just subscribed at Tier 2 ($9.99 USD)! Thank you so freaking much for the support!!! pacopaSmile`;
+				// 	publishMessage(channel, botMsg);
+				// 	break;
 				default:
 					botMsg = `alexiaWow NEW TIER 2 SUB!!! alexiaWow @${user} just subscribed at Tier 2 ($9.99 USD)! Thank you so freaking much for the support!!! alexiaLove`;
 					publishMessage(channel, botMsg);
@@ -185,14 +185,14 @@ async function main() {
 					let botMsg = `spoodHype NEW TIER 3 SUB!!! spoodHype @${user} just subscribed at Tier 3 ($24.99 USD)! THE STREAM IS FREE!!1! alexiaMega`;
 					publishMessage(channel, botMsg);
 					break;
-				case '#jg7tv':
-					botMsg = `jg7tvPog7 NEW TIER 3 SUB!!! jg7tvPog7 @${user} just subscribed at Tier 3 ($24.99 USD)! Thank you so freaking much for the support!!! alexiaLove`;
-					publishMessage(channel, botMsg);
-					break;
-				case '#pacopaco':
-					botMsg = `alexiaWow NEW TIER 3 SUB!!! alexiaWow @${user} just subscribed at Tier 3 ($24.99 USD)! Thank you so freaking much for the support!!! pacopaSmile`;
-					publishMessage(channel, botMsg);
-					break;
+				// case '#jg7tv':
+				// 	botMsg = `jg7tvPog7 NEW TIER 3 SUB!!! jg7tvPog7 @${user} just subscribed at Tier 3 ($24.99 USD)! Thank you so freaking much for the support!!! alexiaLove`;
+				// 	publishMessage(channel, botMsg);
+				// 	break;
+				// case '#pacopaco':
+				// 	botMsg = `alexiaWow NEW TIER 3 SUB!!! alexiaWow @${user} just subscribed at Tier 3 ($24.99 USD)! Thank you so freaking much for the support!!! pacopaSmile`;
+				// 	publishMessage(channel, botMsg);
+				// 	break;
 				default:
 					botMsg = `alexiaWow NEW TIER 3 SUB!!! alexiaWow @${user} just subscribed at Tier 3 ($24.99 USD)! Thank you so freaking much for the support!!! alexiaLove`;
 					publishMessage(channel, botMsg);
@@ -203,19 +203,19 @@ async function main() {
 
   chatClient.onResub((channel, user, subInfo) => {
     if(subInfo.isPrime) {
-      const botMsg = `pokiPrime PRIME RESUB!!! pokiPrime Welcome back @${subInfo.displayName} for ${subInfo.months} months spoodGLB`;
+      const botMsg = `xqcPrime PRIME RESUB!!! xqcPrime Welcome back @${subInfo.displayName} for ${subInfo.months} months kaleiLove`;
       publishMessage(channel, botMsg);
     }
     else if(subInfo.plan === '1000') {
-			const botMsg = `spoodHype RESUB!!! spoodHype @${subInfo.displayName} just resubscribed! Welcome back and thanks for the support <3 spoodGLB`;
+			const botMsg = `spoodHype RESUB!!! spoodHype @${subInfo.displayName} just resubscribed! Welcome back and thanks for the support kaleiLove`;
 			publishMessage(channel, botMsg);
     }
     else if(subInfo.plan === '2000') {
-			const botMsg = `spoodHype RESUB!!! spoodHype @${subInfo.displayName} just resubscribed at Tier 2 ($9.99 USD)! Welcome back and thanks for the support <3 spoodGLB`;
+			const botMsg = `spoodHype RESUB!!! spoodHype @${subInfo.displayName} just resubscribed at Tier 2 ($9.99 USD)! Welcome back and thanks for the support kaleiLove`;
 			publishMessage(channel, botMsg);
     }
     else if(subInfo.plan === '3000') {
-			const botMsg = `spoodHype RESUB!!! spoodHype @${subInfo.displayName} just resubscribed at Tier 3 ($24.99 USD)! Welcome back and thanks for the support <3 spoodGLB`;
+			const botMsg = `spoodHype RESUB!!! spoodHype @${subInfo.displayName} just resubscribed at Tier 3 ($24.99 USD)! Welcome back and thanks for the support kaleiLove`;
 			publishMessage(channel, botMsg);
 		}
   });
@@ -240,7 +240,7 @@ async function main() {
   });
 
   chatClient.onRaid((channel, user, raidInfo) => {
-    const botMsg = `spoodWYA NEW RAID!!! spoodWYA Thank you so much @${raidInfo.displayName} for the ${raidInfo.viewerCount} viewer raid! jg7tvWee`;
+    const botMsg = `spoodWYA NEW RAID!!! spoodWYA Thank you so much @${raidInfo.displayName} for the ${raidInfo.viewerCount} viewer raid! alexiaWow`;
     publishMessage(channel, botMsg);
   });
 
@@ -252,23 +252,23 @@ async function main() {
 			const chnl = channel.trim();
 			switch(chnl) {
 					case '#spoodah':
-						let botMsg = `xqcGift GIFT SUB HYPE!! xqcGift Thank you @${subInfo.gifterDisplayName} for gifting ${subInfo.count} subs to the squadron! alexiaLove`;
+						let botMsg = `alexiaGift GIFT SUB HYPE!! alexiaGift Thank you @${subInfo.gifterDisplayName} for gifting ${subInfo.count} subs to the squadron! alexiaLove`;
 						publishMessage(channel, botMsg);
 						let botMsg2 = `spoodHey Welcome to all the new subscribers! spoodSalute`;
 						publishMessage(channel, botMsg2);
 						break;
-					case '#jg7tv':
-						botMsg = `pokiGift GIFT SUB HYPE!! pokiGift Thank you @${subInfo.gifterDisplayName} for gifting ${subInfo.count} new people into the Sub 7's! jg7tvHype`;
-						publishMessage(channel, botMsg);
-						botMsg2 = `spoodHey Welcome to all the new subscribers! spoodSalute`;
-						publishMessage(channel, botMsg2);
-						break;
-					case '#pacopaco':
-						botMsg = `xqcGift GIFT SUB HYPE!! xqcGift Thank you @${subInfo.gifterDisplayName} for gifting ${subInfo.count} subs to the chat! alexiaLove`;
-						publishMessage(channel, botMsg);
-						botMsg2 = `spoodHey Welcome to all the new subscribers! spoodSalute`;
-						publishMessage(channel, botMsg2);
-						break;
+					// case '#jg7tv':
+					// 	botMsg = `alexiaGift GIFT SUB HYPE!! alexiaGift Thank you @${subInfo.gifterDisplayName} for gifting ${subInfo.count} new people into the Sub 7's! jg7tvHype`;
+					// 	publishMessage(channel, botMsg);
+					// 	botMsg2 = `spoodHey Welcome to all the new subscribers! spoodSalute`;
+					// 	publishMessage(channel, botMsg2);
+					// 	break;
+					// case '#pacopaco':
+					// 	botMsg = `alexiaGift GIFT SUB HYPE!! alexiaGift Thank you @${subInfo.gifterDisplayName} for gifting ${subInfo.count} subs to the chat! alexiaLove`;
+					// 	publishMessage(channel, botMsg);
+					// 	botMsg2 = `spoodHey Welcome to all the new subscribers! spoodSalute`;
+					// 	publishMessage(channel, botMsg2);
+					// 	break;
 					default:
 						botMsg = `alexiaGift GIFT SUB HYPE!! alexiaGift Thank you @${subInfo.gifterDisplayName} for gifting ${subInfo.count} subs to the chat! alexiaLove`;
 						publishMessage(channel, botMsg);
@@ -278,7 +278,7 @@ async function main() {
 				}
 		}
 		else {
-			const botMsg = `alexiaGift RANDOM GIFT SUB HYPE!! alexiaGift Thank you ${subInfo.gifterDisplayName} for gifting a random sub to the chat! spoodLove`;
+			const botMsg = `alexiaGift RANDOM GIFT SUB HYPE!! alexiaGift Thank you ${subInfo.gifterDisplayName} for gifting a random sub to the chat! alexiaLove`;
       publishMessage(channel, botMsg);
 		}
   });
@@ -289,7 +289,7 @@ async function main() {
     if (previousGiftCount > 0) {
       giftCounts.set(user, previousGiftCount - 1);
     } else {
-      const botMsg = `spoodGLB GIFT SUB HYPE!! spoodGLB Thank you ${subInfo.gifterDisplayName} for gifting a sub to ${recipient} spoodLove!`;
+      const botMsg = `spoodGLB GIFT SUB HYPE!! spoodGLB Thank you ${subInfo.gifterDisplayName} for gifting a sub to ${recipient} alexiaLove`;
       publishMessage(channel, botMsg);
     }
 	});
@@ -306,7 +306,7 @@ async function main() {
 	const spoodId = await apiClient.helix.users.getUserByName(twitchChannels[0]) ?? 0; // replace desired index from twitchChannels
 	const logChannel = (await dClient.guilds.fetch('788709812711456798')).channels.cache.get('788711449219694612') as TextChannel;
 
-	const modListener = await pubSubClient.onModAction(userId, spoodId, message => {
+	const modListener = await pubSubClient.onModAction(userId, spoodId, (message: PubSubChatModActionMessage) => {
 		if (message.channelId === spoodId.toString()) {
 			if (message.action === 'timeout') {
 				const [target, duration, reason] = message.args;
@@ -319,10 +319,8 @@ async function main() {
 					.setTimestamp()
 					.addField('Timeout', `${target} was timed out for ${duration} seconds by ${moderator}.`)
 					.addField('Reason', `${reason || 'not specified.'}`);
-				if(logChannel.isText()) {
-					(logChannel as TextChannel).send({ embed: embedVar});
+				logChannel.send({ embed: embedVar});
 				}
-			}
 	
 			if(message.action === 'ban') {
 				const [target, reason] = message.args;
@@ -335,8 +333,7 @@ async function main() {
 					.setTimestamp()
 					.addField('Ban', `${target} was banned by ${moderator}.`)
 					.addField('Reason', `${reason || 'not specified.'}`);
-					//@ts-ignore
-				logChannel.send({ embed: embed});
+				logChannel.send({ embed: embedVar});
 			}
 			
 			if (message.action == 'unban') {
@@ -349,8 +346,7 @@ async function main() {
 					.setColor('0x00ff7f')
 					.setTimestamp()
 					.addField('Unban', `${target} was un-banned by ${moderator}.`);
-				// @ts-ignore
-				logChannel.send({ embed: embed});
+				logChannel.send({ embed: embedVar});
 			}
 	
 			if (message.action == 'untimeout') {
@@ -363,8 +359,7 @@ async function main() {
 					.setColor('0x00ff7f')
 					.setTimestamp()
 					.addField('Unban', `${target} was un-banned by ${moderator}.`);
-				// @ts-ignore
-				logChannel.send({ embed: embed});
+				logChannel.send({ embed: embedVar});
 			}
 		}
 	});
